c36448c0f0fe35c979a27193e5dc2c972e9e5001
Rule1211 <stefan.ruzic1989@gmail.com>
2024-03-11 21:23:18 +0100

    Initial Commit

---
+++ Project21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project21
+//
+//  Created by Stefan Ruzic on 11.3.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
